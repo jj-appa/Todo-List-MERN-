4f3626a8fa2af2946dd3a919ef7e16e15571c645
J_y Wu <jaywupaul@gmail.com>
2026-01-03 14:56:12 -0600

    able to add and display data

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,28 @@
 import { useState } from "react";
+import axios from "axios";
 
 function App() {
-  const [newTask, setNewTask] = useState("");
+  //creating a state to hold the new task input
+  const [newTodo, setNewTodo] = useState("");
+  //creating a state to hold the list of tasks
+  const [todos, setTodos] = useState([]);
+
+  //function to add a new task to the task list
+  const addTodo = async (e) => {
+    //preventing the default form submission behavior
+    e.preventDefault();
+    if (!newTodo.trim()) return;
+    try{
+      const response = await axios.post("/api/todos", {text: newTodo});
+      setTodos([...todos, response.data]);
+      setNewTodo("");
+    } catch (error) {
+      console.log("Error adding todo:", error);
+    }
+  }
+
+  //UI of the App
+  const showTodos = async (e) => {};
 
   return (
     <div className="bg-gradient-to-br from-slate-700 to-slate-800 min-h-screen flex items-center justify-center">
@@ -10,15 +31,16 @@ function App() {
         <form
           class="flex items-center gap-2 shadow-sm border border-gray-200
          rounded-lg p-2"
+         onSubmit={addTodo}
         >
           <input
             className="flex-1 outline-none rounded px-3 py-2 text-gray-700 
             placeholder-gray-400"
             type="text"
-            value={newTask}
-            onChange={(e) => setNewTask(e.target.value)}
+            value={newTodo}
+            onChange={(e) => setNewTodo(e.target.value)}
             placeholder="Add a new task..."
-            required={false}
+            required
           ></input>
           <button
             className="bg-blue-500 hover:bg-blue-600 transition 
@@ -28,7 +50,18 @@ function App() {
             Add Task
           </button>
         </form>
-        <div></div>
+        <div>
+          {/* Task List will go here */}
+          {todos.length === 0 ? (
+            null
+          ): (
+            <div> 
+              {todos.map((todo) => 
+                <div key={todo._id}> {todo.text} </div>)} 
+            </div>
+          )}
+
+        </div>
       </div>
     </div>
   );
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,4 +5,12 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  //Best Practice to hide localhost when dealing with fetching data
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000"
+      }
+    } 
+  }
 });
