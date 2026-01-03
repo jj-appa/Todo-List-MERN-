2b05d6a5649208622cf2196ee469b140c5c104f8
J_y Wu <jaywupaul@gmail.com>
2026-01-03 12:11:39 -0600

    finished backend. Started Frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [newTask, setNewTask] = useState("");
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="bg-gradient-to-br from-slate-700 to-slate-800 min-h-screen flex items-center justify-center">
+      <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-lg p-8">
+        <h1 className="text-3xl font-bold text-gray-800 mb-8">Task Manager</h1>
+        <form
+          class="flex items-center gap-2 shadow-sm border border-gray-200
+         rounded-lg p-2"
+        >
+          <input
+            className="flex-1 outline-none rounded px-3 py-2 text-gray-700 
+            placeholder-gray-400"
+            type="text"
+            value={newTask}
+            onChange={(e) => setNewTask(e.target.value)}
+            placeholder="Add a new task..."
+            required={false}
+          ></input>
+          <button
+            className="bg-blue-500 hover:bg-blue-600 transition 
+            text-white px-4 py-2 rounded-md "
+            type="submit"
+          >
+            Add Task
+          </button>
+        </form>
+        <div></div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
